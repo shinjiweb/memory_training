8aa8138d971b2f1393a93d40323eaa4001514586
Shinji Koyama <koyama.snj@shinjiweb.com>
2019-04-10 21:43:26 +0900

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
 module.exports = {
+  // github pages で公開するための設定
+  publicPath: './',
+  outputDir: 'docs',
+
   css: {
     loaderOptions: {
       sass: {
         data: `@import "@/assets/sass/style.scss";`
       }
     }
-  }
+  },
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined
 }
